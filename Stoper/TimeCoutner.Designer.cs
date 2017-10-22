886480d3a9816edbbf7f1331c108ce33616051dc
KamilRSobczak <32776789+KamilRSobczak@users.noreply.github.com>
2017-10-22 23:26:01 +0200

    Adding Code

---
+++ Stoper/TimeCoutner.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace Stoper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Wymagana metoda obsługi projektanta — nie należy modyfikować 
+        /// zawartość tej metody z edytorem kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Start = new System.Windows.Forms.Button();
+            this.Timer = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Stop = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Scramble = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Start
+            // 
+            this.Start.BackColor = System.Drawing.Color.LightGreen;
+            this.Start.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Start.FlatAppearance.BorderColor = System.Drawing.Color.LimeGreen;
+            this.Start.FlatAppearance.BorderSize = 2;
+            this.Start.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Start.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Start.Location = new System.Drawing.Point(279, 341);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(166, 67);
+            this.Start.TabIndex = 3;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = false;
+            this.Start.Click += new System.EventHandler(this.StartButton);
+            // 
+            // Timer
+            // 
+            this.Timer.AutoSize = true;
+            this.Timer.BackColor = System.Drawing.Color.Transparent;
+            this.Timer.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Timer.Location = new System.Drawing.Point(261, 214);
+            this.Timer.Name = "Timer";
+            this.Timer.Size = new System.Drawing.Size(417, 108);
+            this.Timer.TabIndex = 4;
+            this.Timer.Text = "00:00:00";
+            this.Timer.Click += new System.EventHandler(this.Timer_Numbers);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Stop
+            // 
+            this.Stop.BackColor = System.Drawing.Color.LightGreen;
+            this.Stop.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Stop.FlatAppearance.BorderColor = System.Drawing.Color.LimeGreen;
+            this.Stop.FlatAppearance.BorderSize = 2;
+            this.Stop.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Stop.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Stop.Location = new System.Drawing.Point(480, 341);
+            this.Stop.Name = "Stop";
+            this.Stop.Size = new System.Drawing.Size(166, 67);
+            this.Stop.TabIndex = 5;
+            this.Stop.Text = "Stop";
+            this.Stop.UseVisualStyleBackColor = false;
+            this.Stop.Click += new System.EventHandler(this.StopButton);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(114, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(720, 108);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "CubeStopwatch";
+            // 
+            // Scramble
+            // 
+            this.Scramble.AutoSize = true;
+            this.Scramble.BackColor = System.Drawing.Color.Transparent;
+            this.Scramble.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Scramble.Location = new System.Drawing.Point(233, 172);
+            this.Scramble.Name = "Scramble";
+            this.Scramble.Size = new System.Drawing.Size(413, 24);
+            this.Scramble.TabIndex = 7;
+            this.Scramble.Text = "Scramble: U, U2, U\', D, D2, D\', R, R2, R\', L, L2, L\'";
+            this.Scramble.Click += new System.EventHandler(this.Scramble_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightGreen;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(378, 440);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(166, 67);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Reset";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.ResetButton);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(337, 510);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(270, 16);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Reset = Reset, Pause and ReRoll Scramble";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(963, 556);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Scramble);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Stop);
+            this.Controls.Add(this.Timer);
+            this.Controls.Add(this.Start);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.Label Timer;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button Stop;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Scramble;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Stoper/TimeCoutner.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Stoper
+{
+   
+    public partial class Form1 : Form 
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+        int  min, sec, mSek = 0;
+        string[] algorytmy = { " U ", " U2 ", " U' ", " D ", " D2 ", " D' ", " R ", " R2 ", " R' ", " L ", " L2 ", " L' " };
+
+        //_____________________________________________________________Moduł do odliczania i wyświetlania czasu.
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Timer.Text = String.Format("{0:00}", min) + String.Format("{0::00}", sec)+ String.Format("{0::00}", mSek);
+            
+            if(mSek > 8)
+            {
+                sec++;
+                mSek = 0;
+
+                if (sec > 59)
+                {
+                    min++;
+                    sec = 0;
+
+                    if (min == 60)
+                    {                       
+                        timer1.Stop();
+                        
+                        Timer.Text = "60:00:00";
+                    }
+                } 
+            }
+            else
+            {
+                mSek++;
+                
+            }            
+        }
+
+        //_____________________________________________________________Operacje na przyciskach (Start/Stop/Reset)
+        
+        private void StopButton(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void ResetButton(object sender, EventArgs e)
+        {
+            timer1.Stop();
+
+            sec = 0;
+            mSek = 0;
+            Timer.Text = String.Format("{0:00}", min) + String.Format("{0::00}", sec) + String.Format("{0::00}", mSek);
+
+            
+            Random random = new Random();
+            string rng = " ";
+            for (int i = 0; i < 15; i++)
+            {
+                rng = rng + algorytmy[random.Next(0, 12)];
+            }
+            Scramble.Text = "Scramble: " + rng;
+        }
+
+        private void StartButton(object sender, EventArgs e)
+        {
+            timer1.Start();            
+        }
+        
+        //_____________________________________________________________
+        private void Scramble_Click(object sender, EventArgs e)
+        {
+           
+        }
+        private void Timer_Numbers(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+    
+}
